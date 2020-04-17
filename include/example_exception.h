dfdc8e29ee842f58420cd3d3f5feee57e9b2f577
progrm_jarvis <gh@progrm-jarvis.ru>
2020-04-17 13:55:42 +0300

    Reformat example_exception.h

--- include/example_exception.h
+++ include/example_exception.h
@@ -2,11 +2,12 @@
 #define INCLUDE_EXAMPLE_EXCEPTION_H_
 
 namespace example_exception {
+
     class ExampleException final : public std::runtime_error {
     public:
-        explicit ExampleException(char const* message);
+        explicit ExampleException(char const *message);
 
-        explicit ExampleException(std::string const& message);
+        explicit ExampleException(std::string const &message);
     };
 } // namespace example_exception
 
